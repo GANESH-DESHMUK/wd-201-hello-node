40867cdb83118838c7329be1d22121ccaa4f0f4d
GANESH-DESHMUK <dganesh@vardhaman.org>
2022-09-22 20:32:00 +0530

    fs file unlink

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ fs.rename("sample.txt", "test.txt", (err) => {
   if (err) throw err;
   console.log("File name updated!");
 });
+fs.unlink("test.txt", (err) => {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});

e27531d369594109e61cc9aedcd3635cf10a8f53
GANESH-DESHMUK <dganesh@vardhaman.org>
2022-09-24 19:47:13 +0530

    Read data using minimist to accept cli

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const readline = require("readline");
+const args = require("minimist")(process.argv.slice(2));
 
-const lineDetail = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-lineDetail.question(`Please provide your name - `, (name) => {
-  console.log(`Hi ${name}!`);
-  lineDetail.close();
-});
+console.log(args);

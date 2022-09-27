15dafc8220f2fe81dc32de6f1b68a248737d5d22
GANESH-DESHMUK <dganesh@vardhaman.org>
2022-09-27 21:28:42 +0530

    Included registration.html and tested with milestone submission

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
+let args = require("minimist")(process.argv.slice(2));
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -17,19 +19,36 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(7000);
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+
+  http
+    .createServer((request, response) => {
+      let url = request.url;
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      switch (url) {
+        case "/project":
+          response.write(projectContent);
+          response.end();
+          break;
+        case "/registration":
+          response.write(registrationContent);
+          response.end();
+          break;
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
+      }
+    })
+    .listen(args.port);
+});
